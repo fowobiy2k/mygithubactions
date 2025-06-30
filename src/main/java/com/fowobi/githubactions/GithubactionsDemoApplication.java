1e57248e1121d450c1cbe098af3259b266ea3867
oluwatosin <fowobiy2k@yahoo.com>
2025-06-30 21:25:35 +0100

    added base context

--- src/main/java/com/fowobi/githubactions/GithubactionsDemoApplication.java
+++ src/main/java/com/fowobi/githubactions/GithubactionsDemoApplication.java
@@ -6,11 +6,16 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class GithubactionsDemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(GithubactionsDemoApplication.class, args);
 	}
 
+	@GetMapping
+	public String welcome() {
+		return "You're Welcome";
+	}
 
 }
