02cd3e4b762546b526513b2d0565731aa8963161
oluwatosin <fowobiy2k@yahoo.com>
2025-06-30 08:39:25 +0100

    initial commit

---
+++ src/main/java/com/fowobi/githubactions/GithubactionsDemoApplication.java
@@ -0,0 +1,20 @@
+package com.fowobi.githubactions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class GithubactionsDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GithubactionsDemoApplication.class, args);
+	}
+
+	@GetMapping
+	public String hello() {
+		return "GitHubActions for AWS";
+	}
+}
---
+++ src/test/java/com/fowobi/githubactions/GithubactionsDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fowobi.githubactions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubactionsDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
